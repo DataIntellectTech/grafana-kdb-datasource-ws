0be26a31b397c0cc45bd6fa46c9dbe65a3257b02
7DG <danielrgorrod@gmail.com>
2020-05-23 00:10:50 +0100

    changed maxrowcount to num or str

--- src/model/query-param.ts
+++ src/model/query-param.ts
@@ -1,14 +1,22 @@
 import {QueryDictionary} from "./queryDictionary";
 
+class WhereObject {
+    type: string;
+    datatype: string;
+    name: string
+    params: string[] | string[][]
+}
+
 export class QueryParam {
 
     table: string;
     column: any[] = [];
-    where: string[] = [];
+    //where: WhereObject;
+    where: string[] = []
     temporal_field: any;
     temporal_range: number[] = [];
     grouping: string[] = [];
     conflation: any;
     query: QueryDictionary;
-    maxRowCount: number;
+    maxRowCount: number | string;
 }
