2d6a9dca260ed50f925c86686299886c74110a4b
unknown <daniel.gorrod@aquaq.co.uk>
2020-06-07 22:17:01 +0100

    added metadata to queries

--- src/model/kdb-request.ts
+++ src/model/kdb-request.ts
@@ -2,11 +2,9 @@ import {QueryParam} from "./query-param";
 
 export class KdbRequest {
 
-    time: string;
     refId: string;
     queryId: string;
     query: string;
     queryParam: QueryParam;
     format: string;
-    version: string;
 }
